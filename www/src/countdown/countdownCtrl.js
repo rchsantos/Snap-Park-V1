b4b7143a5969530bb566d75fd70caf9b442be2a9
Richardson Santos <rch.santos@rchwebdesign.ch>
2016-03-29 17:00:12 +0200

    Countdown controller + filter

--- www/src/countdown/countdownCtrl.js
+++ www/src/countdown/countdownCtrl.js
@@ -1,81 +1,64 @@
 angular.module('snapApp')
   .controller('countdownCtrl', function($state, $timeout) {
     console.log('########## Park countdown ##########');
+
       var vm = this;
 
       vm.title = 'Time Parking';
-      vm.thirtyMin = 30;
-      vm.sixtyMin = 60;
-      vm.ninetyMin = 90;
 
-      vm.date = new Date();
+      vm.thirtyMin = 1800;
+      vm.sixtyMin  = 3600;
+      vm.ninetyMin = 5400;
 
-      vm.time = function (date) {
+      vm.time = function () {
         $state.go ('tab.countdown');
       };
 
-    countdownManager = {
+    vm.counter    = 10;
+    vm.stopped    = false;
+    vm.buttonText ='Stop';
 
-      // Configuration
-      targetTime: new Date() + 6000, // Date cible du compte à rebours (00:00:00)
-      displayElement: { // Elements HTML où sont affichés les informations
-        hour: null,
-        min:  null,
-        sec:  null
-      },
+    vm.onTimeout = function() {
+      vm.counter --;
 
-      // Initialisation du compte à rebours (à appeler 1 fois au chargement de la page)
-      init: function(){
-        // Récupération des références vers les éléments pour l'affichage
-        // La référence n'est récupérée qu'une seule fois à l'initialisation pour optimiser les performances
-        this.displayElement.hour = jQuery('#countdown_hour');
-        this.displayElement.min  = jQuery('#countdown_min');
-        this.displayElement.sec  = jQuery('#countdown_sec');
+      timeoutPark = $timeout(vm.onTimeout,1000);
 
-        // Lancement du compte à rebours
-        this.tick(); // Premier tick tout de suite
-        window.setInterval("countdownManager.tick();", 1000); // Ticks suivant, répété toutes les secondes (1000 ms)
-      },
+      if (vm.counter <= 0) {
+        $timeout.cancel(timeoutPark);
+        vm.buttonText = 'Go Back';
 
-      // Met à jour le compte à rebours (tic d'horloge)
-      tick: function(){
-        // Instant présent
-        var timeNow  = new Date();
-
-        // On s'assure que le temps restant ne soit jamais négatif (ce qui est le cas dans le futur de targetTime)
-        if( timeNow > this.targetTime ){
-          timeNow = this.targetTime;
+        vm.goBack = function () {
+          $state.go('tab.countdownSett');
         }
-
-        // Calcul du temps restant
-        var diff = this.dateDiff(timeNow, this.targetTime);
-
-        this.displayElement.day.text(  diff.day  );
-        this.displayElement.hour.text( diff.hour );
-        this.displayElement.min.text(  diff.min  );
-        this.displayElement.sec.text(  diff.sec  );
-      },
-
-      // Calcul la différence entre 2 dates, en jour/heure/minute/seconde
-      dateDiff: function(date1, date2){
-        var diff = {}                           // Initialisation du retour
-        var tmp = date2 - date1;
-
-        tmp = Math.floor(tmp/1000);             // Nombre de secondes entre les 2 dates
-        diff.sec = tmp % 60;                    // Extraction du nombre de secondes
-        tmp = Math.floor((tmp-diff.sec)/60);    // Nombre de minutes (partie entière)
-        diff.min = tmp % 60;                    // Extraction du nombre de minutes
-        tmp = Math.floor((tmp-diff.min)/60);    // Nombre d'heures (entières)
-        diff.hour = tmp % 24;                   // Extraction du nombre d'heures
-        tmp = Math.floor((tmp-diff.hour)/24);   // Nombre de jours restants
-        diff.day = tmp;
-
-        return diff;
       }
     };
 
-    // Lancement du compte à rebours au chargement de la page
-    //  countdownManager.init();
+    vm.timeoutPark = $timeout(vm.onTimeout,1000);
 
+    vm.takeAction = function(){
 
-  });
+      if(!vm.stopped) {
+        $timeout.cancel(timeoutPark);
+        vm.buttonText = 'Resume';
+      } else {
+        timeoutPark = $timeout(vm.onTimeout,1000);
+        vm.buttonText='Stop';
+      }
+
+      vm.stopped = !vm.stopped;
+    }
+
+  })
+
+.filter('formatTimer', function() {
+
+  return function(input)
+
+  {
+    function z(n) {return (n < 10 ? '0' : '') + n;}
+    var seconds = input % 60;
+    var minutes = Math.floor( input / 60);
+    var hours = Math.floor( minutes / 60);
+    return (z(hours) +':'+z(minutes)+':'+z(seconds));
+  };
+});
--- www/src/maps/trouveMoiCtrl.js
+++ www/src/maps/trouveMoiCtrl.js
@@ -24,8 +24,8 @@ angular.module('snapApp')
     });
 
     // Options du map
-     var mapOptions = {
-      center: { lat: 46.204677, lng: 6.143106 },
+    var mapOptions = {
+      center: {lat: 46.204677, lng: 6.143106},
       zoom: 16,
       disableDefaultUI: true,
       mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -39,22 +39,21 @@ angular.module('snapApp')
 
     // init maps geolocation
 
-      // Finalize le loader
-      $ionicLoading.hide();
+    // Finalize le loader
+    $ionicLoading.hide();
 
-      var directionsDisplay = new google.maps.DirectionsRenderer;
-      var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    var directionsService = new google.maps.DirectionsService;
 
-      vm.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    vm.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-      directionsDisplay.setMap(vm.map);
+    directionsDisplay.setMap(vm.map);
 
 
-      var btnMarker = document.getElementsByClassName('mdl-button');
+    var btnMarker = document.getElementsByClassName('mdl-button');
 
 
     function calculateRoute(from, to, $rootScope) {
-
 
 
       var directionsService = new google.maps.DirectionsService();
@@ -68,10 +67,8 @@ angular.module('snapApp')
 
       directionsService.route(
         directionsRequest,
-        function(response, status)
-        {
-          if (status == google.maps.DirectionsStatus.OK)
-          {
+        function (response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
             new google.maps.DirectionsRenderer({
               map: vm.map,
               directions: response
@@ -85,89 +82,83 @@ angular.module('snapApp')
     }
 
 
-    $(document).ready(function() {
+    $("#from-link, #to-link").click(function (event) {
+
+      event.preventDefault();
+
+      // Address google maps
+      var addressId = this.id.substring(0, this.id.indexOf("-"));
 
 
-        $("#from-link, #to-link").click(function (event) {
+      navigator.geolocation.getCurrentPosition(function (position) {
 
-          event.preventDefault();
+          var geocoder = new google.maps.Geocoder();
 
-          // Address google maps
-          var addressId = this.id.substring(0, this.id.indexOf("-"));
+          geocoder.geocode({
+              "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+            },
+            function (results, status) {
 
+              if (status == google.maps.GeocoderStatus.OK) {
 
-          navigator.geolocation.getCurrentPosition(function (position) {
-
-              var geocoder = new google.maps.Geocoder();
-
-              geocoder.geocode({
-                  "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-                },
-                function (results, status) {
-
-                  if (status == google.maps.GeocoderStatus.OK) {
-
-                    // New position
-                    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                // New position
+                var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
 
-                    // Creation new Marker
-                    marker = new google.maps.Marker({
-                      map: vm.map,
-                      draggable: true,
-                      animation: google.maps.Animation.DROP,
-                      position: latLng
-                    });
-
-                    function clearMarkers() {
-                      setMapOnAll(null);
-                    }
-
-                    // Display the new Marker with infoWindow
-                    $timeout(function(){
-                      console.log('Enter in Marker');
-                      vm.map.setCenter(marker.getPosition());
-
-                      // InfoWindow
-                      $timeout(function() {
-                        infoWindow.open(vm.map, marker);
-                      }, 500);
-                    }, 500);
-
-                    console.log('Position Marker: ' + marker.getPosition);
-
-                    $('#from-link').removeClass('fadeInLeft').addClass('fadeOutRight');
-                    $('#to-link').show().addClass('fadeInLeft');
-
-
-
-                    $("#" + addressId).val(results[0].formatted_address);
-
-
-
-                  } else
-                    $("#error").append("Unable to retrieve your address<br />");
+                // Creation new Marker
+                marker = new google.maps.Marker({
+                  map: vm.map,
+                  draggable: true,
+                  animation: google.maps.Animation.DROP,
+                  position: latLng
                 });
 
-            },
-            function (positionError) {
-              $("#error").append("Error: " + positionError.message + "<br />");
-            },
-            {
-              enableHighAccuracy: true,
-              timeout: 10 * 1000 // 10 seconds
+                function clearMarkers() {
+                  setMapOnAll(null);
+                }
+
+                // Display the new Marker with infoWindow
+                $timeout(function () {
+                  console.log('Enter in Marker');
+                  vm.map.setCenter(marker.getPosition());
+
+                  // InfoWindow
+                  $timeout(function () {
+                    infoWindow.open(vm.map, marker);
+                  }, 500);
+                }, 500);
+
+                console.log('Position Marker: ' + marker.getPosition);
+
+                $('#from-link').removeClass('fadeInLeft').addClass('fadeOutRight');
+                $('#to-link').show().addClass('fadeInLeft');
+
+
+                $("#" + addressId).val(results[0].formatted_address);
+
+
+              } else
+                $("#error").append("Unable to retrieve your address<br />");
             });
 
+        },
+        function (positionError) {
+          $("#error").append("Error: " + positionError.message + "<br />");
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 10 * 1000 // 10 seconds
         });
 
-        $("#calculate-route").submit(function (event) {
-          event.preventDefault();
-          calculateRoute($("#from").val(), $("#to").val());
+    });
 
-          // Clear Markers Firts positions
-          marker.setMap(null);
-          console.log('Marker Initial deleted');
-        });
-      });
+    $("#calculate-route").submit(function (event) {
+      event.preventDefault();
+      calculateRoute($("#from").val(), $("#to").val());
+
+      // Clear Markers Firts positions
+      marker.setMap(null);
+      console.log('Marker Initial deleted');
+    });
 
   });
